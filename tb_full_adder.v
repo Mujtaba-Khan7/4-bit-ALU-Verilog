4cfe1d320fe53aa6cb35a15a3625c01061f55fca
Mujtaba <127471852+Mujtaba-Khan7@users.noreply.github.com>
2026-06-15 19:16:37 +0530

    Add testbench for full adder module

---
+++ tb_full_adder.v
@@ -0,0 +1,24 @@
+module tb_full_adder;
+  reg  a, b, cin;
+  wire sum, cout;
+
+  full_adder DUT (.a(a), .b(b), .cin(cin), .sum(sum), .cout(cout));
+
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0, tb_full_adder);
+
+    $monitor("a=%b b=%b cin=%b | sum=%b cout=%b", a, b, cin, sum, cout);
+
+    a=0; b=0; cin=0; #10;
+    a=0; b=0; cin=1; #10;
+    a=0; b=1; cin=0; #10;
+    a=0; b=1; cin=1; #10;
+    a=1; b=0; cin=0; #10;
+    a=1; b=0; cin=1; #10;
+    a=1; b=1; cin=0; #10;
+    a=1; b=1; cin=1; #10;
+
+    $finish;
+  end
+endmodule
